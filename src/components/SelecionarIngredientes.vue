78383f65db17112a7f7635ae6a10416951df3a31
lmoraesdev <lbc_moraes@outlook.com>
2023-11-21 19:53:28 -0300

    feat: cardCategoria

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { obterCategorias } from "@/http/index";
 import type ICategoria from "@/interfaces/ICategoria";
+import CardCategoria from "./CardCategoria.vue";
 export default {
   data() {
     return {
@@ -10,6 +11,7 @@ export default {
   async created() {
     this.categorias = await obterCategorias();
   },
+  components: { CardCategoria },
 };
 </script>
 
@@ -23,7 +25,7 @@ export default {
 
     <ul class="categorias">
       <li v-for="categoria in categorias" :key="categoria.nome">
-        {{ categoria.nome }}
+        <CardCategoria :categoria="categoria" />
       </li>
     </ul>
 
